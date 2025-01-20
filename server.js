1b323d19326572b3f9a3e1f42a183710e21015b5
Robin Kamo <robin.kamo@iths.se>
2025-01-20 11:48:05 +0100

    added connectiing to mongodb

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const PORT = 3000;
 
 
-app.listen(PORT, () => {
-    console.log("listening to 3000");
+app.get("/", (req, res) => {
+    res.send("Welcome to my mongoAPI")
+})
+
+app.post("/blogpost", (req, res) => {
+    res.send("this is a postrequest")
+});
+
+
+mongoose.
+connect("mongodb+srv://kakashidota:123456admin@robinsapi.8jc0x.mongodb.net/Products-API?retryWrites=true&w=majority&appName=RobinsAPI")
+.then(() => {
+    console.log("connected to mongo");
+    app.listen(PORT, () => {
+        console.log("listening to 3000");
+    });
+  
 });
